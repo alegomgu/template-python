870d773e2e9b4a4c16aefeb7228219247b4dc609
alegomgu <61535863+alegomgu@users.noreply.github.com>
2023-11-21 11:36:44 +0100

    Pruebas

--- server.py
+++ server.py
@@ -1,7 +1,58 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configuración de la base de datos
+DB_USER = 'tfl0user'
+DB_PASS = '4qWeDjYl1ixT'
+DB_HOST = 'ep-billowing-sun-46121554.eu-central-1.aws.neon.fl0.io'
+DB_NAME = 'database'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?sslmode=require'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Modelo de Datos
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    edad = db.Column(db.Integer)
+
+class Mascota(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    tipo = db.Column(db.String(50))
+
+# Función para inicializar la base de datos con registros de ejemplo
+def init_db():
+    db.create_all()  # Crea las tablas si no existen
+
+    # Verificar si la base de datos está vacía
+    if not Persona.query.first():
+        personas = [
+            Persona(nombre="Carlos", edad=30),
+            Persona(nombre="Lucía", edad=25)
+        ]
+
+        mascotas = [
+            Mascota(nombre="Max", tipo="Perro"),
+            Mascota(nombre="Mia", tipo="Gato")
+        ]
+
+        db.session.add_all(personas + mascotas)
+        db.session.commit()
+
+@app.route('/personas')
+def get_personas():
+    personas = Persona.query.all()
+    return jsonify([{'id': p.id, 'nombre': p.nombre, 'edad': p.edad} for p in personas])
+
+@app.route('/mascotas')
+def get_mascotas():
+    mascotas = Mascota.query.all()
+    return jsonify([{'id': m.id, 'nombre': m.nombre, 'tipo': m.tipo} for m in mascotas])
 
 port = int(os.environ.get("PORT", 5000))
 
@@ -22,4 +73,6 @@ def hola():
    return 'Hola'
 
 if __name__ == "__main__":
+    with app.app_context():
+        init_db()
     app.run(port=port)
